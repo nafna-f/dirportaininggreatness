1477e88d608705cfb1f1cf913bc6b23171482970
Naf (Nafiyu Murtaza) <nmurtaza50@stuy.edu>
2024-09-23 11:35:07 -0400

    guh

--- 08_teardown/app.py
+++ 08_teardown/app.py
@@ -5,12 +5,12 @@ DISCO:
 <note any discoveries you made here... no m-atter how small!>
 
 QCC:
-0. 
-1. 
-2. 
-3. 
-4. 
-5. 
+0. Mainly java, C.
+1. Your computer, the "root".
+2. The root / your local host.
+3. "No hablo queso!"
+4. It appears on the local host interface. I know this because we tested it.
+5. Java, the arrays attribute for length and how py is using .run().
  ...
 
 INVESTIGATIVE APPROACH:
@@ -22,14 +22,14 @@ INVESTIGATIVE APPROACH:
 
 from flask import Flask
 
-app = Flask(__name__)                    # Q0: Where have you seen similar syntax in other langs? 
-
-@app.route("/")                          # Q1: What points of reference do you have for meaning of '/'?
+app = Flask(__name__)                    # Q0: Where have you seen similar syntax in other langs? Mainly java, C.
+                                                    
+@app.route("/")                          # Q1: What points of reference do you have for meaning of '/'? Your computer, the "root".
 def hello_world():
-    print(__name__)                      # Q2: Where will this print to?
-                                         # Q3: What will it print?
-    return "No hablo queso!"             # Q4: Will this appear anywhere? How u know?
+    print(__name__)                      # Q2: Where will this print to? The root / your local host.
+                                         # Q3: What will it print? "No hable queso!"
+    return "No hablo queso!"             # Q4: Will this appear anywhere? How u know? it.
 
-app.run()                                # Q5: Where have you seen similar constructs in other languages?
+app.run()                                # Q5: Where have you seen similar constructs in other languages? 
 
 
