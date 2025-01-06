30117b94358d3fd4197c6a7bfca11691dde36338
Naf (Nafiyu Murtaza) <nmurtaza50@stuy.edu>
2025-01-06 11:42:59 -0500

    started on 27

---
+++ 27_js/fibfac.js
@@ -0,0 +1,37 @@
+//Team IDunno :: Naf, Princeden
+//SoftDev Pd06
+//K27 - Basic functions in JavaScript
+//2025-01-06m
+
+//JavaScript implementations of Day0 recursive Scheme functions
+
+//factorial:
+
+let fact = function(n){
+	if (n == 1){
+		return 1;
+	}else{
+		return (n * fact(n - 1));
+	}
+}
+
+//TEST CALLS
+
+//fact(1)
+//fact(2)
+//fact(3)
+//fact(4)
+//fact(5)
+
+//-----------------------------------------------------------------
+
+
+//fib:
+
+//<your team's fib(n) implementation>
+
+//TEST CALLS
+// (writing here can facilitate EZer copy/pasting into dev console now and later...)
+
+//=================================================================
+
